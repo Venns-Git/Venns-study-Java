5571089412c40144af8d31842b31c1d93f900653
Venns-Git <venns-email@foxmail.com>
2020-12-08 21:13:41 +0800

    ssmbuild

---
+++ ssmbuild/src/main/java/com/venns/service/BookService.java
@@ -0,0 +1,21 @@
+package com.venns.service;
+
+import com.venns.pojo.Books;
+import java.util.List;
+
+public interface BookService {
+    //增加一本书
+    int addBook(Books books);
+
+    //删除一本书
+    int deleteById(int id);
+
+    //修改一本书
+    int updateBook(Books books);
+
+    //查询一本书
+    Books queryBookById(int id);
+
+    //查询全部的书
+    List<Books> queryAllBook();
+}
---
+++ ssmbuild/src/main/java/com/venns/service/BookServiceImpl.java
@@ -0,0 +1,41 @@
+package com.venns.service;
+
+import com.venns.dao.BookMapper;
+import com.venns.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+
+    //service层调dao层
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    @Override
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    @Override
+    public int deleteById(int id) {
+        return bookMapper.deleteById(id);
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    @Override
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
